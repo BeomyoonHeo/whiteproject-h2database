3c2516dd8f9674a8530db57a77cc2b63c258e1e3
BeomYoonHeo <gqd0512@naver.com>
2022-10-21 12:41:17 +0900

    DB콘솔 세팅 완료

--- src/main/java/site/metacoding/white/domain/Board.java
+++ src/main/java/site/metacoding/white/domain/Board.java
@@ -3,6 +3,7 @@ package site.metacoding.white.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,7 +13,7 @@ import lombok.Setter;
 @Entity
 public class Board {
     @Id// - primary key 적용
-    @GeneratedValue// - auto increment 적용
+    @GeneratedValue(strategy=GenerationType.IDENTITY)// - auto increment 적용
     private Long id;
     private String title;
     @Column(length=1000)

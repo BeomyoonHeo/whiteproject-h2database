6da340c7951d5247161d5662d752beef609475bc
BeomYoonHeo <gqd0512@naver.com>
2022-10-27 15:53:41 +0900

    JPA Repository사용해서 기본CRUD 선언

---
+++ src/main/java/site/metacoding/white/domain/UserJpaRepository.java
@@ -0,0 +1,13 @@
+package site.metacoding.white.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserJpaRepository extends JpaRepository<User, Long> {
+
+    //Jpa named  query
+    
+    @Query(value="select u from User u where u.username = :username")
+    User findByUsername(@Param("username") String username);
+}

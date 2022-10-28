8239ce45573a9c643a4399763372f5921c1b8f11
BeomYoonHeo <gqd0512@naver.com>
2022-10-28 10:51:59 +0900

    로그인 테스트 완료

--- src/test/java/site/metacoding/white/web/UserApiControllerTest.java
+++ src/test/java/site/metacoding/white/web/UserApiControllerTest.java
@@ -2,7 +2,6 @@ package site.metacoding.white.web;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,6 +21,8 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 
 import site.metacoding.white.dto.UserRequestDto.JoinReqDto;
+import site.metacoding.white.dto.UserRequestDto.LoginReqDto;
+import site.metacoding.white.service.UserService;
 
 @ActiveProfiles("test")
 // 테스트에서 중요한것은 테스트 격리이다. - 테스트 후에 다시 테스트를 진행할 때 같은 값으로 테스트 진행시에 테스트에 이상이 없어야 한다.
@@ -33,18 +34,22 @@ public class UserApiControllerTest {
     @Autowired
     private TestRestTemplate rt; // 통신시 사용하는 라이브러리
 
-    private static ObjectMapper om;
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private UserService userService;
+
     private static HttpHeaders headers;
 
     @BeforeAll // 선언시 static으로 선언해야한다. - container에 띄우기 위해 사용한다.
     public static void init() {
-        om = new ObjectMapper();
         headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
     }
     
     
-    @Order(1)
+    //@Order(1)
     @Test
     public void join_test() throws JsonProcessingException {
         // given
@@ -53,7 +58,6 @@ public class UserApiControllerTest {
         joinReqDto.setPassword("1234");
 
         String body = om.writeValueAsString(joinReqDto);
-        System.out.println(body);
 
         // when
         HttpEntity<String> request = new HttpEntity<>(body, headers);
@@ -70,29 +74,66 @@ public class UserApiControllerTest {
         Assertions.assertThat(code).isEqualTo(1);
     }
 
-    @Order(2)
     @Test
-    public void join_test2() throws JsonProcessingException {
-        // given
+    public void login_test() throws JsonProcessingException {
+        //data init
         JoinReqDto joinReqDto = new JoinReqDto();
         joinReqDto.setUsername("very");
         joinReqDto.setPassword("1234");
+        userService.save(joinReqDto); // join_test메서드에서 검증이 이미 끝났기 때문에 굳이 return값을 받아서 테스트를 진행 할 필요가 없다.
 
-        String body = om.writeValueAsString(joinReqDto);
-        System.out.println(body);
 
-        // when
+        // given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("very");
+        loginReqDto.setPassword("1234");
+        String body = om.writeValueAsString(loginReqDto);
+        System.out.println("디버그: " + body);
+
+        //when
         HttpEntity<String> request = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST,
+        ResponseEntity<String> response = rt.exchange("/login", HttpMethod.POST,
                 request, String.class);
 
-        // then
-        // System.out.println(response.getStatusCode());
-        // System.out.println(response.getBody());
+        System.out.println("디버그 : " + response.getBody());
 
+        //then
         DocumentContext dc = JsonPath.parse(response.getBody());
         // System.out.println(dc.jsonString());
         Integer code = dc.read("$.code");
+        String username = dc.read("$.data.username");
+        String msg = dc.read("$.msg");
+        Integer id = dc.read("$.data.id");
         Assertions.assertThat(code).isEqualTo(1);
+        Assertions.assertThat(username).isEqualTo("very");
+        Assertions.assertThat(msg).isEqualTo("ok");
+        Assertions.assertThat(id).isEqualTo(1);
+
     }
+
+    // //@Order(2)
+    // @Test
+    // public void join_test2() throws JsonProcessingException {
+    //     // given
+    //     JoinReqDto joinReqDto = new JoinReqDto();
+    //     joinReqDto.setUsername("very");
+    //     joinReqDto.setPassword("1234");
+
+    //     String body = om.writeValueAsString(joinReqDto);
+    //     System.out.println(body);
+
+    //     // when
+    //     HttpEntity<String> request = new HttpEntity<>(body, headers);
+    //     ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST,
+    //             request, String.class);
+
+    //     // then
+    //     // System.out.println(response.getStatusCode());
+    //     // System.out.println(response.getBody());
+
+    //     DocumentContext dc = JsonPath.parse(response.getBody());
+    //     // System.out.println(dc.jsonString());
+    //     Integer code = dc.read("$.code");
+    //     Assertions.assertThat(code).isEqualTo(1);
+    // }
 }

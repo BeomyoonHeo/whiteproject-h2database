db38c65d59f93be62998661680cf493681cfe5ee
BeomYoonHeo <gqd0512@naver.com>
2022-10-28 10:01:40 +0900

    h2데이터베이스로 springboot 통합테스트 완료

--- src/main/java/site/metacoding/white/domain/User.java
+++ src/main/java/site/metacoding/white/domain/User.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity //Value Object
 @NoArgsConstructor
+@Table(name = "users") // truncate.sql의 테이블은 user를 사용할 수 없기때문에 users로 설정을 진행하였다.
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/site/metacoding/white/web/UserApiControllerTest.java
+++ src/test/java/site/metacoding/white/web/UserApiControllerTest.java
@@ -2,6 +2,7 @@ package site.metacoding.white.web;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,7 +24,10 @@ import com.jayway.jsonpath.JsonPath;
 import site.metacoding.white.dto.UserRequestDto.JoinReqDto;
 
 @ActiveProfiles("test")
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+// 테스트에서 중요한것은 테스트 격리이다. - 테스트 후에 다시 테스트를 진행할 때 같은 값으로 테스트 진행시에 테스트에 이상이 없어야 한다.
+//@Transactional//테스트 트랜잭션 진행 후 자동으로 롤백
+@Sql("classpath:truncate.sql") // truncate.sql은 자동으로 table을 empty상태로 만들어 준다. - 내부 메서드가 종료되면 자동으로 실행 해준다.
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT) //Spring Boot 통합테스트 진행시 자동으로 롤백 해주지 않는다. - @Transactional이 먹히지 않는다.
 public class UserApiControllerTest {
 
     @Autowired
@@ -38,29 +43,56 @@ public class UserApiControllerTest {
         headers.setContentType(MediaType.APPLICATION_JSON);
     }
     
-
+    
+    @Order(1)
     @Test
     public void join_test() throws JsonProcessingException {
-        //given
+        // given
         JoinReqDto joinReqDto = new JoinReqDto();
-        joinReqDto.setUsername("papa");
+        joinReqDto.setUsername("very");
         joinReqDto.setPassword("1234");
 
         String body = om.writeValueAsString(joinReqDto);
-        //System.out.println(body);
+        System.out.println(body);
 
-        //when
+        // when
         HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST,
+                request, String.class);
 
-        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST, request, String.class);
-
-        //then
+        // then
         // System.out.println(response.getStatusCode());
         // System.out.println(response.getBody());
 
         DocumentContext dc = JsonPath.parse(response.getBody());
+        // System.out.println(dc.jsonString());
         Integer code = dc.read("$.code");
         Assertions.assertThat(code).isEqualTo(1);
     }
 
+    @Order(2)
+    @Test
+    public void join_test2() throws JsonProcessingException {
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("very");
+        joinReqDto.setPassword("1234");
+
+        String body = om.writeValueAsString(joinReqDto);
+        System.out.println(body);
+
+        // when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST,
+                request, String.class);
+
+        // then
+        // System.out.println(response.getStatusCode());
+        // System.out.println(response.getBody());
+
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        // System.out.println(dc.jsonString());
+        Integer code = dc.read("$.code");
+        Assertions.assertThat(code).isEqualTo(1);
+    }
 }
